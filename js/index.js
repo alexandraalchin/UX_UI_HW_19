76cc0dba50f07b83fc0644b61a658ba67ff011fe
alexandraalchin <alchinalexandra@gmail.com>
2021-12-06 18:14:45 -0700

    Not working
    CSS issues

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-jQuery( "#content" ).ready(function() {
+jQuery( "#intro" ).ready(function() {
     jQuery( ".slow" ).first().show( "slow", function showNext() {
       jQuery( this ).next( "div" ).show( "slow", showNext );
     });
