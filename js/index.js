fb2201cf33538e77b12b85b08f6c9aa8be2afeb3
alexandraalchin <alchinalexandra@gmail.com>
2021-12-06 17:29:56 -0700

    Ugh trying things
    not working

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-jQuery( ".content" ).ready(function() {
+jQuery( "#content" ).ready(function() {
     jQuery( ".slow" ).first().show( "slow", function showNext() {
       jQuery( this ).next( "div" ).show( "slow", showNext );
     });
